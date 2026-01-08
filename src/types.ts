0772c6b30745550bda9c39a6ea8dcb5bb2af4ea7
Federico Secco <federico.secco7@gmail.com>
2026-01-08 22:50:14 +0100

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,7 @@ import type { ConversationFlavor } from "@grammyjs/conversations";
 import type { I18nFlavor } from "@grammyjs/i18n";
 import type { Context } from "grammy";
 
-export type BotContext = Context & ConversationFlavor & I18nFlavor;
+export type BotContext = Context & ConversationFlavor<Context> & I18nFlavor;
 export type ConversationContext = Context & I18nFlavor;
 
 export type UserInput = {
